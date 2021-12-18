73345a64fbefd3d77fceed1849bbd52fd0b57b49
phronesys <daniel.dazarola@gmail.com>
2021-12-18 18:33:29 -0300

    feat: navigation bar layout

---
+++ src/layouts/NavBar.jsx
@@ -0,0 +1,12 @@
+import "./NavBar.css";
+import Filter from "../components/Filter";
+import InputSearch from "../components/InputSearch";
+
+export default function NavBar() {
+  return (
+    <nav className="navigation">
+      <Filter></Filter>
+      <InputSearch></InputSearch>
+    </nav>
+  );
+}
