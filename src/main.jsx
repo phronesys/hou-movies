133530709ba623642a99b99eae5209e9c172859b
phronesys <daniel.dazarola@gmail.com>
2021-12-18 18:31:35 -0300

    chore: tailwind config

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
+
+/* const API_KEY = import.meta.env.API_KEY;
+
+if (!API_KEY) {
+  console.error('[error]: The "API_KEY" environment variable is required');
+}
+ */
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
-  content: [],
+  content: [
+    './index.html',
+    './src/**/*.{html,js,jsx}',
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "houm-primary": "#ff452b",
+        "houm-secondary": "#fa8072",
+        "houm-dark": "#263238",
+      },
+    },
   },
   plugins: [],
 }
