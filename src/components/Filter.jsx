3edb08257d1b61e2e8183b767dc89070c3087467
phronesys <daniel.dazarola@gmail.com>
2021-12-19 01:41:48 -0300

    refactor: filter needs redux to save his state?

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,28 +1,18 @@
 import "./Filter.css";
 import { useState } from "react";
 
-export default function Filter({ selected, addSelectedIndex }) {
+export default function Filter() {
   const [isOpen, setIsOpen] = useState(false);
-
-  const list = () => {
-    return movieGenres.map((genre, index) => {
-      const checked = selected.includes(genre.id);
-      console.log(checked);
-      return (
-        <li key={index}>
-          <label>
-            <input
-              type="checkbox"
-              checked={checked}
-              onChange={() => addSelectedIndex(genre.id)}
-            />
-            {selected.includes(genre.id)}
-            <div>{genre.type}</div>
-          </label>
-        </li>
-      );
-    });
+  const [selected, setSelected] = useState(
+    new Array(movieGenres.length).fill(false)
+  );
+  console.log(selected);
+  const updateSelected = (value, index) => {
+    const newSelected = selected;
+    newSelected[index] = value;
+    setSelected(newSelected);
   };
+
   return (
     <div className="filter">
       <button
@@ -31,7 +21,26 @@ export default function Filter({ selected, addSelectedIndex }) {
       >
         Filter
       </button>
-      {isOpen && <ul className="list">{list()}</ul>}
+      {isOpen && (
+        <ul className="list">
+          {movieGenres.map((genre, index) => {
+            return (
+              <li key={index}>
+                <label>
+                  <input
+                    type="checkbox"
+                    checked={selected[index]}
+                    onChange={(event) =>
+                      updateSelected(event.target.checked, index)
+                    }
+                  />
+                  <div>{genre.type}</div>
+                </label>
+              </li>
+            );
+          })}
+        </ul>
+      )}
     </div>
   );
 }
