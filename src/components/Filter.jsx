3aaff6abec90d59f173282071bd0569cf3564f3c
phronesys <daniel.dazarola@gmail.com>
2021-12-18 22:56:38 -0300

    bug: I cant update child when parent changes

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,33 +1,27 @@
 import "./Filter.css";
 import { useState } from "react";
 
-export default function Filter({ selected, setSelected }) {
+export default function Filter({ selected, addSelectedIndex }) {
   const [isOpen, setIsOpen] = useState(false);
-  const addSelectedIndex = (id) => {
-    console.log(selected);
-    if (!Array.isArray(selected)) return;
-    const indexOfSelected = selected.indexOf(id);
-    const newSelected = selected;
-    if (indexOfSelected === -1) newSelected.push(id)
-    else newSelected.splice(indexOfSelected, 1)
-    console.log(newSelected);
-    setSelected(newSelected);
-  };
 
   const list = () => {
-    return movieGenres.map((genre, index) => (
-      <li key={index}>
-        <label>
-          <input
-            type="checkbox"
-            checked={selected.includes(genre.id)}
-            onChange={() => addSelectedIndex(genre.id)}
-          />
-          {selected.includes(genre.id)}
-          <div>{genre.type}</div>
-        </label>
-      </li>
-    ));
+    return movieGenres.map((genre, index) => {
+      const checked = selected.includes(genre.id);
+      console.log(checked);
+      return (
+        <li key={index}>
+          <label>
+            <input
+              type="checkbox"
+              checked={checked}
+              onChange={() => addSelectedIndex(genre.id)}
+            />
+            {selected.includes(genre.id)}
+            <div>{genre.type}</div>
+          </label>
+        </li>
+      );
+    });
   };
   return (
     <div className="filter">
--- src/layouts/NavBar.jsx
+++ src/layouts/NavBar.jsx
@@ -1,14 +1,21 @@
 import "./NavBar.css";
 import Filter from "../components/Filter";
 import InputSearch from "../components/InputSearch";
-import { useState } from "react"
+import { useState } from "react";
 
 export default function NavBar() {
   const [selected, setSelected] = useState([]);
-  
+  const newSelected = selected;
+  const addSelectedIndex = (id) => {
+    if (!Array.isArray(selected)) return;
+    const indexOfSelected = selected.indexOf(id);
+    if (indexOfSelected === -1) newSelected.push(id);
+    else newSelected.splice(indexOfSelected, 1);
+    setSelected(newSelected);
+  };
   return (
     <nav className="navigation">
-      <Filter selected={selected} setSelected={setSelected}></Filter>
+      <Filter selected={selected} addSelectedIndex={addSelectedIndex}></Filter>
       <InputSearch></InputSearch>
     </nav>
   );
