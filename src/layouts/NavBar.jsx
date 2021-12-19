4a38b953576f07cc7b06927e4877410622282186
phronesys <daniel.dazarola@gmail.com>
2021-12-18 22:42:03 -0300

    enhance: two way binding navbar/filter

--- src/layouts/NavBar.jsx
+++ src/layouts/NavBar.jsx
@@ -1,11 +1,14 @@
 import "./NavBar.css";
 import Filter from "../components/Filter";
 import InputSearch from "../components/InputSearch";
+import { useState } from "react"
 
 export default function NavBar() {
+  const [selected, setSelected] = useState([]);
+  
   return (
     <nav className="navigation">
-      <Filter></Filter>
+      <Filter selected={selected} setSelected={setSelected}></Filter>
       <InputSearch></InputSearch>
     </nav>
   );
