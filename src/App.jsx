e49ccb2b9202b86f3281513b5dcb70e8f48ad37c
phronesys <daniel.dazarola@gmail.com>
2021-12-19 18:55:35 -0300

    enhance: load api data

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
-import './App.css'
+import "./App.css";
 import NavBar from "./layouts/NavBar";
+import getMovieList from "./services/getMovieList";
+import { useEffect, useState } from "react";
+
+const moviesUrl = getMovieList("popular");
 
 function App() {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    const getMovies = async () => {
+      const res = await fetch(moviesUrl)
+        .then((res) => res.json())
+        .catch((error) => console.log(error));
+      setData(res.results);
+    };
+    getMovies();
+  }, []);
+
+  console.log(data);
+
   return (
     <div className="main">
       <NavBar></NavBar>
-
+      {data.length > 0 ? data.map((movie) => <div key={movie.id}>{movie.title}</div>) : ""}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
