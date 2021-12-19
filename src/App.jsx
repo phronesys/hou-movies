48ef8cdc43e6512cda762fd19ed6985a9ed987d9
phronesys <daniel.dazarola@gmail.com>
2021-12-19 02:12:59 -0300

    refactor: unusued useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-import { useState } from 'react'
 import './App.css'
 import NavBar from "./layouts/NavBar";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="main">
       <NavBar></NavBar>
