4c9490f4415d265bb86466a3952f661c26079d7f
phronesys <daniel.dazarola@gmail.com>
2021-12-19 01:41:10 -0300

    enhance: navbar doesnt have to manage child logic

--- src/layouts/NavBar.jsx
+++ src/layouts/NavBar.jsx
@@ -1,22 +1,14 @@
 import "./NavBar.css";
 import Filter from "../components/Filter";
 import InputSearch from "../components/InputSearch";
-import { useState } from "react";
 
 export default function NavBar() {
-  const [selected, setSelected] = useState([]);
-  const newSelected = selected;
-  const addSelectedIndex = (id) => {
-    if (!Array.isArray(selected)) return;
-    const indexOfSelected = selected.indexOf(id);
-    if (indexOfSelected === -1) newSelected.push(id);
-    else newSelected.splice(indexOfSelected, 1);
-    setSelected(newSelected);
-  };
   return (
     <nav className="navigation">
-      <Filter selected={selected} addSelectedIndex={addSelectedIndex}></Filter>
+      <Filter></Filter>
       <InputSearch></InputSearch>
     </nav>
   );
 }
+
+
